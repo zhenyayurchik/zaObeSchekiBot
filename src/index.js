3dc139f954b5e098fb08b0604e777f9ae3dfd6fc
Eugene Yurchik <zhenyayurchik@gmail.com>
2021-03-19 14:01:54 +0300

    v8

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const kb = require("./keyboard-buttons");
 const keyboard = require("./keyboard");
 const { getChatId, getItemUuid } = require("./helper");
 const token = `${process.env.BOT_TOKEN}`;
-// const database = require("../database.json");
+const database = require("../database.json");
 
 require("./models/film.model");
 require("./models/cinema.model");
