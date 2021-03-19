f3577d44e588c146346bff3868b7e515d6ab0afd
Eugene Yurchik <zhenyayurchik@gmail.com>
2021-03-19 12:53:57 +0300

    v2

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+process.env.NTBA_FIX_319 = 1;
 const TelegramBot = require("node-telegram-bot-api");
 const mongoose = require("mongoose");
 const geolib = require("geolib");
